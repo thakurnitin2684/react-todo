3f9ef7bd152c97e319707eb994dcad9a96e1d1de
thakurnitin2684 <thakurnitin2684@gmail.com>
2022-06-13 21:15:23 +0530

    Updates

---
+++ precache-manifest.2f3935ab52b6612ca2f610354ee7b3a5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2c0c64f0a87ceae791416090fe4d5c9",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "4b5af5504815c98dce2a",
+    "url": "/react-todo/static/js/2.ee650b07.chunk.js"
+  },
+  {
+    "revision": "28b5e048a5dc26d06754",
+    "url": "/react-todo/static/js/main.a7323efa.chunk.js"
+  },
+  {
+    "revision": "d59dc2bb74734947adf2",
+    "url": "/react-todo/static/js/runtime-main.5324b8f9.js"
+  }
+]);
